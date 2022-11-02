66c36348b88bf74fee75133aea9b93d2f5dddaf7
Suchanad kongpon <108140712+kkwanzy@users.noreply.github.com>
2022-11-02 22:05:25 +0700

    add theme

--- app.js
+++ app.js
@@ -2,17 +2,21 @@ const express = require ('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const path = require('path');
+
 const app = express();
 const port = 5000;
 
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname,"/Public/")));
+
 app.get("/",(req,res)=>{
 
     res.send('Hello nong beam');
 })
 
 app.listen(port,()=>{
-    debug("Moo beam live in the wolrd on port " + chalk.green(port));
+    debug("Moo beam live in the wolrd on port " + chalk.green(" : "+port));
 })
 
 
